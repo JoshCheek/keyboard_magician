27549da1160266cf2e2a67e1f61f8935bc3cf6d6
Josh Cheek <josh.cheek@gmail.com>
2014-02-01 22:55:47 +0000

    Some basic character interpretation

---
+++ spec/interpret_character_spec.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require 'interpret_character'
+
+describe InterpretCharacter do
+  def translate(char)
+    InterpretCharacter.call char
+  end
+
+  it 'translates inputs that can be represented as characters to their symbol' do
+    characters = [
+      *'a'..'z',
+      *'A'..'Z',
+      *'0'..'9',
+      "'",
+      *'`~!@#$%^&*()_+-=[]{}\\|;:"<>,./?œ∑´®†'.chars,
+    ]
+
+    characters.each do |char|
+      expect(translate char).to eq [char.intern]
+    end
+  end
+
+  it 'translates "\u007F" as :delete' do
+    expect(translate "\u007F").to eq [:delete]
+  end
+
+  describe 'arrow keys' do
+    it('translates \e[A as :up')    { expect(translate "\e[A").to eq [:up   ] }
+    it('translates \e[B as :down')  { expect(translate "\e[B").to eq [:down ] }
+    it('translates \e[C as :right') { expect(translate "\e[C").to eq [:right] }
+    it('translates \e[D as :left')  { expect(translate "\e[D").to eq [:left ] }
+  end
+
+end
