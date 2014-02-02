0287085016f883b1bbbafa671bb8263540f9a9bb
Josh Cheek <josh.cheek@gmail.com>
2014-02-02 00:24:38 +0000

    Irrelevant changes to the smoke test

--- spec/smoke_spec.rb
+++ spec/smoke_spec.rb
@@ -7,13 +7,9 @@ describe 'Hitting the binary just to make sure shit works' do
     end
   end
 
-  def execute(command, stdin_data)
-    Invocation.new *Open3.capture3(command, stdin_data: stdin_data)
-  end
-
   it 'works' do
-    bin = File.expand_path '../../bin/tutor', __FILE__
-    invocation = execute(bin, 'abc de1')
+    bin        = File.expand_path '../../bin/tutor', __FILE__
+    invocation = Invocation.new *Open3.capture3(bin, stdin_data: 'abc de9')
     expect(invocation.stderr).to eq String.new
     expect(invocation.stdout).to include "num errors:            1"
     expect(invocation.status).to eq 0
