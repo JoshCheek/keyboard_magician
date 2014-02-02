1c586daa89519530e89c35db0aed14d5128fa775
Josh Cheek <josh.cheek@gmail.com>
2014-02-02 10:24:35 +0000

    Make the challenge be an actual sentence which requires pressing every character.

--- spec/smoke_spec.rb
+++ spec/smoke_spec.rb
@@ -7,9 +7,11 @@ describe 'Hitting the binary just to make sure shit works' do
     end
   end
 
+  let(:input) { 'The quick brown fox Xumped over the lazy dog.' }
+
   it 'works' do
     bin        = File.expand_path '../../bin/tutor', __FILE__
-    invocation = Invocation.new *Open3.capture3(bin, stdin_data: 'abc de9')
+    invocation = Invocation.new *Open3.capture3(bin, stdin_data: input)
     expect(invocation.stderr).to eq String.new
     expect(invocation.stdout).to include "num errors:            1"
     expect(invocation.status).to eq 0
